6b33e0ea2ca066c893eb72f458d1d3a1ae76101e
Labyrinth246 <77074418+Labyrinth246@users.noreply.github.com>
2023-01-13 13:26:07 -0700

    started adding the header and functionality (#3)

---
+++ BakerPracticeFrontend/src/components/contact.js
@@ -0,0 +1,13 @@
+import React from "react";
+import contact from "./contact.module.css";
+import Header from "../header";
+
+function Contact() {
+    return (
+        <div>
+            <Header/>
+        </div>
+    );
+}
+
+export default Contact;
