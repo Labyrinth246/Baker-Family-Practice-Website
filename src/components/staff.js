396f1331181ce840d3fb9bed4d8d3c2b075642d6
Spencer Baker <bak17018@byui.edu>
2023-11-01 20:01:09 -0600

    redid staff page

--- src/components/staff.js
+++ src/components/staff.js
@@ -1,114 +1,148 @@
-import React from "react";
+import { React, useState} from "react";
 import staff from "./staff.module.css";
 import Header from "../header";
 import Background from "../background";
 
 function Staff() {
+
+    const [flip, setFlip] = useState(false);
+
+    const flipCard = () => {
+        setFlip(!flip);
+        console.log(flip);
+    }
+
     return (
         <div className={staff.container}>
             <Header/>
             <div className={staff.staff}>
                 
-                {/*Left side of background*/}
-                <div className={staff.leftSide}>
+                
+                <div className={staff.staffCards}>
 
-                    <div className={staff.picture}>
-                        <img className={staff.staffPictures}
-                            src="/images/nopic.png"
-                            alt="staffpic"/>
-                        <h3>Wallace C. Baker MD</h3>
-                        <div className={staff.bio}>
-                            <p></p>
+                    {/*Page Title*/}
+                    <div className={staff.title}>
+                        <h1>Meet Your Caretakers!</h1>
+                    </div>
+
+                    {/*whitespace div*/}
+                    <div className={staff.whitespace}></div>
+
+
+                    {/*Staff Cards*/}
+
+                    {/*sidespace div*/}
+                    <div className={staff.sidespaceOne}></div>
+
+                    {/*sidespace div*/}
+                    <div className={staff.sidespaceTwo}></div>
+
+                    <div className={staff.picture} >
+                        <div className={staff.front} onClick={() => {flipCard();}}>
+                            <img className={staff.staffPictures}
+                                src="/images/staff/Wallace.jpg"
+                                alt="staffpic"/>
+                            <h3>Wallace C. Baker MD</h3>
                         </div>
+
+                        <div className={staff.bio} onClick={() => {flipCard();}}> <p></p> </div>
+
                     </div>
 
                     <div className={staff.picture}>
-                        <img className={staff.staffPictures}
-                            src="/images/nopic.png"
-                            alt="staffpic"/>
-                        <h3>Wade Christensen PA-C</h3>
-                        <div className={staff.bio}>
-                            <p></p>
+                        <div className={staff.front} onClick={() => {flipCard();}}>
+                            <img className={staff.staffPictures}
+                                src="/images/staff/Wade.jpg"
+                                alt="staffpic"/>
+                            <h3>Wade Christensen PA-C</h3>
                         </div>
+
+                        <div className={staff.bio} onClick={() => {flipCard();}}> <p></p> </div>
+
                     </div>
 
                     <div className={staff.picture}>
-                        <img className={staff.staffPictures}
-                            src="/images/nopic.png"
-                            alt="staffpic"/>
-                        <h3>Becky Newby PA-C</h3>
-                        <div className={staff.bio}>
-                            <p></p>
+                        <div className={staff.front} onClick={() => {flipCard();}}>
+                            <img className={staff.staffPictures}
+                                src="/images/staff/Becky.jpg"
+                                alt="staffpic"/>
+                            <h3>Becky Newby PA-C</h3>
                         </div>
+
+                        <div className={staff.bio} onClick={() => {flipCard();}}> <p></p> </div>
+
                     </div>
 
                     <div className={staff.picture}>
-                        <img className={staff.staffPictures}
-                            src="/images/nopic.png"
-                            alt="staffpic"/>
-                        <h3>Josh Hanson PA-C</h3>
-                        <div className={staff.bio}>
-                            <p></p>
+                        <div className={staff.front} onClick={() => {flipCard();}}>
+                            <img className={staff.staffPictures}
+                                src="/images/staff/Josh.jpg"
+                                alt="staffpic"/>
+                            <h3>Josh Hanson PA-C</h3>
                         </div>
+
+                        <div className={staff.bio} onClick={() => {flipCard();}}> <p></p> </div>
+
                     </div>
 
                     <div className={staff.picture}>
-                        <img className={staff.staffPictures}
-                            src="/images/nopic.png"
-                            alt="staffpic"/>
-                        <h3>Jessica Christensen NP</h3>
-                        <div className={staff.bio}>
-                            <p></p>
+                        <div className={staff.front} onClick={() => {flipCard();}}>
+                            <img className={staff.staffPictures}
+                                src="/images/staff/nopicG.jpg"
+                                alt="staffpic"/>
+                            <h3>Jessica Christensen NP</h3>
                         </div>
-                    </div>
-                </div>
 
-                {/*Middle Title*/}
-                <div className={staff.title}>
-                    <h1>Staff Page</h1>
-                </div>
+                        <div className={staff.bio} onClick={() => {flipCard();}}> <p></p> </div>
 
-                {/*Right side of background*/}
-                <div className={staff.rightSide}>
-
-                    <div className={staff.picture}>
-                        <img className={staff.staffPictures}
-                            src="/images/nopic.png"
-                            alt="staffpic"/>
-                        <h3>Carol Parker CMA</h3>
-                        <div className={staff.bio}>
-                            <p></p>
-                        </div>
                     </div>
 
                     <div className={staff.picture}>
-                        <img className={staff.staffPictures}
-                            src="/images/nopic.png"
-                            alt="staffpic"/>
-                        <h3>Ashley Roberts CMA</h3>
-                        <div className={staff.bio}>
-                            <p></p>
+                        <div className={staff.front} onClick={() => {flipCard();}}>
+                            <img className={staff.staffPictures}
+                                src="/images/staff/Carol.jpg"
+                                alt="staffpic"/>
+                            <h3>Carol Parker CMA</h3>
                         </div>
+
+                        <div className={staff.bio} onClick={() => {flipCard();}}> <p></p> </div>
+
                     </div>
 
                     <div className={staff.picture}>
-                        <img className={staff.staffPictures}
-                            src="/images/nopic.png"
-                            alt="staffpic"/>
-                        <h3>Erin Clifford CMA<br/>Billing Manager</h3>
-                        <div className={staff.bio}>
-                            <p></p>
+                        <div className={staff.front} onClick={() => {flipCard();}}>
+                            <img className={staff.staffPictures}
+                                src="/images/staff/Ashley.jpg"
+                                alt="staffpic"/>
+                            <h3>Ashley Roberts CMA</h3>
                         </div>
+                        
+                        <div className={staff.bio} onClick={() => {flipCard();}}> <p></p> </div>
+
                     </div>
 
                     <div className={staff.picture}>
-                        <img className={staff.staffPictures}
-                            src="/images/nopic.png"
-                            alt="staffpic"/>
-                        <h3>Tina Rojas<br/>Front Desk Manager</h3>
-                        <div className={staff.bio}>
-                            <p></p>
+                        <div className={staff.front} onClick={() => {flipCard();}}>
+                            <img className={staff.staffPictures}
+                                src="/images/staff/nopicO.jpg"
+                                alt="staffpic"/>
+                            <h3>Erin Clifford CMA<br/>Billing Manager</h3>
                         </div>
+
+                        <div className={staff.bio} onClick={() => {flipCard();}}> <p></p> </div>
+
+                    </div>
+
+                    <div className={staff.picture}>
+                        <div className={staff.front} onClick={() => {flipCard();}}>
+                            <img className={staff.staffPictures}
+                                src="/images/staff/nopicG.jpg"
+                                alt="staffpic"/>
+                            <h3>Tina Rojas<br/>Front Desk Manager</h3>
+                        </div>
+
+                        <div className={staff.bio} onClick={() => {flipCard();}}> <p></p> </div>
+                        
                     </div>
                 </div>
 
